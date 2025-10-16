3e845f3ccf22ac4d9e8b9e24d275b720186752c5
RezaMaulanaPutra <2300018253@webmail.uad.ac.id>
2025-10-16 07:11:13 +0700

    Initial commit

---
+++ app/src/main/java/com/example/post2_2300018253/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.post2_2300018253
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.ViewCompat
+import android.view.View // Impor View jika Anda perlu mengakses properti View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById<View>(android.R.id.content)) { view, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            view.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
